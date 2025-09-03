5d5f89d5d591e82961fb66937cfc23e9b31b2607
gaminghusky1 <Justin_Ma@outlook.com>
2025-09-02 20:50:02 -0700

    Add TeleOpExample for club fair. Need to double check that signs are correct and that velocity scale is correct (should be 50% right now).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/GoBildaPinpointOdometry.java
@@ -0,0 +1,81 @@
+// Implementation of the OdometryModule interface for SparkFunOTOS sensor.
+
+package org.firstinspires.ftc.teamcode.components;
+
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.drivers.GoBildaPinpointDriver;
+import org.firstinspires.ftc.teamcode.system.OdometryModule;
+
+public class GoBildaPinpointOdometry implements OdometryModule {
+    private final GoBildaPinpointDriver pinpoint;
+    private Pose2D position;
+    private int positionPriority;
+    private int headingPriority;
+    private boolean doPositionReset;
+    private boolean doHeadingReset;
+    public GoBildaPinpointOdometry(GoBildaPinpointDriver pinpoint) {
+        this.pinpoint = pinpoint;
+
+        positionPriority = 3;
+        headingPriority = 2;
+        doPositionReset = true;
+        doHeadingReset = false;
+    }
+
+    public Pose2D getPosition() {
+        return position;
+    }
+
+    public void updatePosition() {
+        position = pinpoint.getPosition();
+    }
+
+    public void setPosition(Pose2D position) {
+        pinpoint.setPosition(position);
+    }
+
+    public void setPositionPriority(int priority) {
+        positionPriority = priority;
+    }
+
+    public int getPositionPriority() {
+        return positionPriority;
+    }
+
+    public void setHeadingPriority(int priority) {
+        headingPriority = priority;
+    }
+
+    public int getHeadingPriority() {
+        return headingPriority;
+    }
+
+    public void setDoPositionResetToHigherPriority(boolean doReset) {
+        doPositionReset = doReset;
+    }
+
+    public boolean doPositionResetToHigherPriority() {
+        return doPositionReset;
+    }
+
+    public void setDoHeadingResetToHigherPriority(boolean doReset) {
+        doHeadingReset = doReset;
+    }
+
+    public boolean doHeadingResetToHigherPriority() {
+        return doHeadingReset;
+    }
+
+    public boolean isPositionAccurate() {
+        return true;
+    }
+
+    public boolean isHeadingAccurate() {
+        return true;
+    }
+
+    public void reset() {
+        pinpoint.resetPosAndIMU();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TeleOpExample.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.system.BasicHolonomicDrivetrain;
+
+@Config
+@TeleOp(name = "TeleOp Example", group = "TeleOp")
+public class TeleOpExample extends OpMode {
+    public static final double MAX_VELOCITY = 1500;
+    private BasicHolonomicDrivetrain driveTrain;
+    private double lastTime;
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+        driveTrain = new BasicHolonomicDrivetrain(
+                hardwareMap.get(DcMotorEx.class, "backLeft"),
+                hardwareMap.get(DcMotorEx.class, "backRight"),
+                hardwareMap.get(DcMotorEx.class, "frontLeft"),
+                hardwareMap.get(DcMotorEx.class, "frontRight")
+        );
+    }
+
+    @Override
+    public void loop() {
+        // Set velocity targets based on gamepad input
+        driveTrain.setVelocityDrive(
+                -gamepad1.left_stick_y * MAX_VELOCITY,
+                -gamepad1.left_stick_x * MAX_VELOCITY,
+                gamepad1.right_stick_x * MAX_VELOCITY);
+        // Make the motors reach target velocities
+        driveTrain.drive();
+    }
+}
